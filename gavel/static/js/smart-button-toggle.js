02ad188dadf39a47ae03bc5fe3a3e2ceecde3f69
wickathou <javier@enddesign.co>
2024-06-20 15:22:21 +0300

    Added submit button dynamic tag update when project or skip option are selected Modified vote layout rendering to use project names instead of previous and current tags

--- gavel/static/js/smart-button-toggle.js
+++ gavel/static/js/smart-button-toggle.js
@@ -1,5 +1,18 @@
-function buttonSelected() {
-  document.getElementById('vote-submit').disabled = false;
+function buttonSelected(event) {
+  const radioButton = event.target;
+
+  // Find the associated label using the 'for' attribute
+  const label = document.querySelector(`label[for="${radioButton.id}"]`);
+  const voteSubmit = document.getElementById('vote-submit');
+
+  if (radioButton.value === 'Skip') {
+    voteSubmit.value = 'Confirm Skip';
+  } else if (label) {
+    // Retrieve the text content of the label
+    const labelText = label.textContent.trim();
+    voteSubmit.value = `Vote for ${labelText}`
+  }
+  voteSubmit.disabled = false;
 }
 
 (function() {
